4c3844b71c4162366519aba215af13d0decd9d24
Prashanth Badri <101384067+Prashanth-Badri@users.noreply.github.com>
2023-11-29 10:07:00 +0530

    Update banker_S.c

--- banker_S.c
+++ banker_S.c
@@ -1,71 +1,94 @@
- #include <stdio.h>
-int main() { 
-int num_processes, num_resources; 
-printf("Enter the number of processes: "); 
-scanf("%d", &num_processes); 
-printf("Enter the number of resources: "); 
-scanf("%d", &num_resources); 
+#include <stdio.h>
 
-int available[num_resources]; 
-int max[num_processes][num_resources]; 
-int allocation[num_processes][num_resources]; 
-int need[num_processes][num_resources]; 
-int work[num_resources]; 
-int finish[num_processes]; 
-printf("Enter the available resources: "); 
-for (int i = 0; i < num_resources; i++) { 
-scanf("%d", &available[i]); 
- } 
-printf("Enter the maximum resources for each process:\n"); 
-for (int i = 0; i < num_processes; i++) { 
-for (int j = 0; j < num_resources; j++) { 
-scanf("%d", &max[i][j]); 
- } 
- } 
-printf("Enter the allocated resources for each process:\n"); 
-for (int i = 0; i < num_processes; i++) { 
-for (int j = 0; j < num_resources; j++) { 
-scanf("%d", &allocation[i][j]); 
-need[i][j] = max[i][j] - allocation[i][j]; 
- } 
-finish[i] = 0; 
- } 
-// Initialize the work vector to be the available vector
-for (int i = 0; i < num_resources; i++) { 
-work[i] = available[i]; 
- } 
-int safe_sequence[num_processes]; 
-int count = 0;
- 
-printf("allocatoed resources for each process:\n"); 
-for (int i = 0; i < num_processes; i++) { 
-for (int j = 0; j < num_resources; j++) { 
-printf("%d " , allocation[i][j]); 
- } 
-printf("\n") ;
- } 
-printf("the Need resources for each process:\n"); 
-for (int i = 0; i < num_processes; i++) { 
-for (int j = 0; j < num_resources; j++) { 
-printf("%d " , need[i][j]); 
- } 
-printf("\n") ;
- } 
+#define MAX_RESOURCES 10
+#define MAX_PROCESSES 10
 
-printf("the maximum resources for each process:\n"); 
-for (int i = 0; i < num_processes; i++) { 
-for (int j = 0; j < num_resources; j++) { 
-printf("%d ",  max[i][j]); 
- } 
-printf("\n") ;
- } 
+// Data structures for Banker's Algorithm
+int available[MAX_RESOURCES];
+int maximum[MAX_PROCESSES][MAX_RESOURCES];
+int allocation[MAX_PROCESSES][MAX_RESOURCES];
+int need[MAX_PROCESSES][MAX_RESOURCES];
 
-for (int i = 0; i < num_processes; i++) { 
-//for (int j = 0; j < num_resources; j++) { 
-printf("%d ",  available[i]); 
-  
- }
-printf("\n") ;
+// Function to initialize data structures
+void initializeDataStructures(int numResources, int numProcesses) {
+    // Initialize available resources
+    printf("Enter available resources:\n");
+    for (int i = 0; i < numResources; i++) {
+        printf("Resource %d: ", i + 1);
+        scanf("%d", &available[i]);
+    }
 
-return 0; 
+    // Initialize maximum demand matrix
+    printf("\nEnter maximum demand matrix:\n");
+    for (int i = 0; i < numProcesses; i++) {
+        printf("For Process %d:\n", i + 1);
+        for (int j = 0; j < numResources; j++) {
+            printf("Resource %d: ", j + 1);
+            scanf("%d", &maximum[i][j]);
+        }
+    }
+
+    // Initialize allocation matrix
+    printf("\nEnter allocation matrix:\n");
+    for (int i = 0; i < numProcesses; i++) {
+        printf("For Process %d:\n", i + 1);
+        for (int j = 0; j < numResources; j++) {
+            printf("Resource %d: ", j + 1);
+            scanf("%d", &allocation[i][j]);
+
+            // Calculate need matrix
+            need[i][j] = maximum[i][j] - allocation[i][j];
+        }
+    }
+}
+
+// Function to display data in table format
+void displayTable(int numResources, int numProcesses) {
+    printf("\nTable Format:\n");
+
+    // Display header
+    printf("\nProc\t\tAlloc\t\tMax\t\t\tNeed\n");
+   // for (int i = 0; i < numResources; i++) {
+    //    printf("Resource %d  ", i + 1);
+   // }
+    printf("\n");
+
+    // Display data
+    for (int i = 0; i < numProcesses; i++) {
+        printf("P%d\t\t", i + 1);
+
+        // Display Allocation
+        for (int j = 0; j < numResources; j++) {
+            printf("%d  ", allocation[i][j]);
+        }
+
+        // Display Maximum
+        printf("\t\t");
+        for (int j = 0; j < numResources; j++) {
+            printf("%d  ", maximum[i][j]);
+        }
+
+        // Display Need
+        printf("\t\t");
+        for (int j = 0; j < numResources; j++) {
+            printf("%d  ", need[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+int main() {
+    int numResources, numProcesses;
+
+    printf("Enter the number of resources: ");
+    scanf("%d", &numResources);
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &numProcesses);
+
+    initializeDataStructures(numResources, numProcesses);
+    displayTable(numResources, numProcesses);
+
+    return 0;
 }
